a3a1d577282db4d6d63881dc514352a4230bc891
Abdullashaheen10 <abdullashaheen936@gmail.com>
2026-02-08 13:49:28 +0200

    Add files via upload

--- Lesson/Lesson.cs
+++ Lesson/Lesson.cs
@@ -1,42 +1,80 @@
-namespace WebApplication4.Lesson
+namespace WebApplication5.Lesson
 {
     public class Lesson
     {
         public int Id { get; set; }
-        public int TrackId { get; set; }
-        public string TrackName { get; set; } = "";
         public string Title { get; set; } = "";
-        public string DriveUrl { get; set; } = "";
+        public string CourseLink { get; set; } = "";
+        public List<LessonItem> Items { get; set; } = new List<LessonItem>();
+    }
+
+    public class LessonItem
+    {
+        public int Id { get; set; }
+        public int LessonId { get; set; }
+        public string Title { get; set; } = "";
+        public string Url { get; set; } = "";
     }
 
     public static class LessonStore
     {
-        public static List<Lesson> Lessons = new List<Lesson>
+        private static int _nextLessonId = 4;
+        private static int _nextItemId = 1;
+        public static List<Lesson> Lessons { get; } = new List<Lesson>
         {
             new Lesson
             {
                 Id = 1,
-                TrackId = 1,
-                TrackName = "Backend .NET",
-                Title = "Introduction to .NET",
-                DriveUrl = "https://drive.google.com/your-drive-link-here"
+                Title = "IT Course",
+                CourseLink = "https://drive.google.com/drive/folders/it-course-placeholder"
             },
             new Lesson
             {
                 Id = 2,
-                TrackId = 1,
-                TrackName = "Backend .NET",
-                Title = "Entity Framework Basics",
-                DriveUrl = "https://drive.google.com/your-drive-link-here"
+                Title = "Backend .NET",
+                CourseLink = "https://drive.google.com/drive/folders/backend-dotnet-placeholder"
             },
             new Lesson
             {
                 Id = 3,
-                TrackId = 2,
-                TrackName = "Frontend",
-                Title = "HTML & CSS",
-                DriveUrl = "https://drive.google.com/your-drive-link-here"
+                Title = "Career & Interview Skills",
+                CourseLink = "https://drive.google.com/drive/folders/career-interview-placeholder"
             }
         };
+
+        public static Lesson? GetById(int id) => Lessons.FirstOrDefault(l => l.Id == id);
+
+        public static void Add(Lesson lesson)
+        {
+            lesson.Id = _nextLessonId++;
+            lesson.Items = new List<LessonItem>();
+            Lessons.Add(lesson);
+        }
+
+        public static bool Delete(int id)
+        {
+            var lesson = GetById(id);
+            if (lesson == null) return false;
+            Lessons.Remove(lesson);
+            return true;
+        }
+
+        public static void AddItem(int lessonId, LessonItem item)
+        {
+            var lesson = GetById(lessonId);
+            if (lesson == null) return;
+            item.Id = _nextItemId++;
+            item.LessonId = lessonId;
+            lesson.Items.Add(item);
+        }
+
+        public static bool DeleteItem(int lessonId, int itemId)
+        {
+            var lesson = GetById(lessonId);
+            var item = lesson?.Items.FirstOrDefault(i => i.Id == itemId);
+            if (item == null) return false;
+            lesson!.Items.Remove(item);
+            return true;
+        }
     }
 }
--- Lesson/LessonsController.cs
+++ Lesson/LessonsController.cs
@@ -1,17 +1,63 @@
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplication4.Lesson
+namespace WebApplication5.Lesson
 {
     public class LessonsController : Controller
     {
-        public IActionResult Index(int trackId = 1)
+        public IActionResult Index()
         {
-            var lessons = LessonStore.Lessons.Where(l => l.TrackId == trackId).ToList();
-            if (!lessons.Any())
-                return NotFound();
-            ViewBag.TrackName = lessons.First().TrackName;
-            return View(lessons);
+            return View(LessonStore.Lessons);
+        }
+
+        [HttpGet]
+        public IActionResult AddCourse()
+        {
+            return View(new Lesson());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddCourse(Lesson lesson)
+        {
+            if (string.IsNullOrWhiteSpace(lesson.Title)) lesson.Title = "Lesson";
+            LessonStore.Add(lesson);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCourse(int id)
+        {
+            LessonStore.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult New(int lessonId)
+        {
+            var lesson = LessonStore.GetById(lessonId);
+            if (lesson == null) return NotFound();
+            ViewBag.Lesson = lesson;
+            return View(new LessonItem { LessonId = lessonId });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult New(int lessonId, LessonItem item)
+        {
+            var lesson = LessonStore.GetById(lessonId);
+            if (lesson == null) return NotFound();
+            item.LessonId = lessonId;
+            LessonStore.AddItem(lessonId, item);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteItem(int lessonId, int itemId)
+        {
+            LessonStore.DeleteItem(lessonId, itemId);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
